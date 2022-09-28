82a75660b3d9cc8f74e9ea1df6de5b2ad385f762
ngayo <ngayobenedict7@gmail.com>
2022-09-28 18:24:10 +0300

    swiper tech styles

--- src/comps/Technology/technology.jsx
+++ src/comps/Technology/technology.jsx
@@ -6,38 +6,51 @@ import "swiper/css/pagination";
 import "./technology.css";
 const { technology } = require("../../data.json");
 const Technology = () => {
+  let nums = [1, 2, 3];
   return (
-    <div>
+    <div className="flex-col items-center pt-32 pl-32 bg-technologyDesktop h-screen text-white">
       <div>
-        <h1>
-          <span>03 </span> scace launch 101
+        <h1 className="font-barlow text-xl uppercase font-bold tracking-wide">
+          <span className="opacity-25">03 </span> scace launch 101
         </h1>
       </div>
       <Swiper
         modules={[Pagination]}
         pagination={{
           clickable: true,
-          el: ".swiper-pagination2",
-          type: "bullets",
+          el: ".swiper-pagination3",
+          renderBullet: function (index, className) {
+            return (
+              '<span  class="' + className + '">' + nums[index] + "</span>"
+            );
+          },
         }}
       >
         {technology.map(
           ({ name, images: { portrait, horizontal }, description }) => {
             return (
               <SwiperSlide>
-                <section>
-                  <div>
-                    <div className="swiper-pagination3"></div>
-                    <div>
-                      <h3>the technnology...</h3>
-                      <h1>{name}</h1>
-                      <p>{description}</p>
+                <section className="flex justify-between content-center ">
+                  <div className="flex justify-between  ">
+                    <div className="swiper-pagination3 "></div>
+                    <div className="">
+                      <h3 className="uppercase font-Barlow text-[#D0D6F9] text-base ">
+                        the terminology...
+                      </h3>
+                      <h1 className="font-body uppercase text-[56px] font-normal ">
+                        {name}
+                      </h1>
+                      <p className="text-[#D0D6F9] font-normal font-Barlow w-[470px] ">
+                        {description}
+                      </p>
                     </div>
                   </div>
-                  <div>
-                    <div>
-                      <img src={portrait} alt="technology" />
-                    </div>
+                  <div className="w-1/3 h-1/3">
+                    <img
+                      src={portrait}
+                      alt="technology"
+                      className="aspect-square"
+                    />
                   </div>
                 </section>
               </SwiperSlide>
